e19ec329651883edcc597498f9a579b16ce4aedc
ivangonz <ivanezeq.gonzalez@gmail.com>
2025-03-09 17:36:37 -0300

    MVP0: Terminado. Se agregan rutas y react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import AllUsers from './views/AllUsers.tsx'
+import UserDetails from './views/UserDetails.tsx'
+import PostUser from './views/PostUser.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path='/allUsers' element={<AllUsers />}/>
+        <Route path='/userDetails' element={<UserDetails />}/>
+        <Route path='/postUser' element={<PostUser />}/>
+        <Route path='/*' element={<App />}/>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
---
+++ src/views/AllUsers.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AllUsers = () => {
+  return <div>All users page</div>
+}
+
+export default AllUsers;
