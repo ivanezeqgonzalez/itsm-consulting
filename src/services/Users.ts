69d715a51024cc66b4c92ba79b4c73b6afad4543
ivangonz <ivanezeq.gonzalez@gmail.com>
2025-03-10 12:26:22 -0300

    ITSM-6 Consumir el service y mostrar el detalle del User.

--- src/services/Users.ts
+++ src/services/Users.ts
@@ -3,3 +3,7 @@ import { axios } from './axiosConfig'
 export const getUsers = () => {
   return axios.get('https://jsonplaceholder.typicode.com/users')
 }
+
+export const getUser = (id: string) => {
+  return axios.get('https://jsonplaceholder.typicode.com/users/' + id)
+}
--- src/views/UserDetails.tsx
+++ src/views/UserDetails.tsx
@@ -1,25 +1,54 @@
-import { useNavigate } from 'react-router'
-import { USERS_MOCK } from './mocks/users'
+import { useNavigate, useParams } from 'react-router'
 import CardUserDetail from '../components/CardUserDetail/CardUserDetail'
 import { Button } from '@mui/material'
 import { POSTS_MOCK } from './mocks/posts'
 import ContainerPosts from '../components/ContainerPosts/ContainerPosts'
+import { useEffect, useState } from 'react'
+import { IUser } from '../domain/User'
+import { getUser } from '../services/Users'
 
 const UserDetails = () => {
   const navigate = useNavigate()
+  const [user, setUser] = useState<IUser | null>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const { id } = useParams()
+
   const handleBack = () => {
     navigate('/allUsers')
+  }
+
+  useEffect(() => {
+    if (!id) return
+    getUser(id).then((response) => {
+      setUser(response.data)
+      setLoading(() => false)
+    })
+  }, [])
+
+  if (id === undefined) {
+    return (
+      <div>
+        <h1>Detalles del usuario</h1>
+        <h3>No se ha proporcionado un ID</h3>
+      </div>
+    )
   }
 
   return (
     <div>
       <h1>Detalles del usuario</h1>
-      <CardUserDetail user={USERS_MOCK[0]} />
-      <h2>Posteos</h2>
-      <ContainerPosts posts={POSTS_MOCK} />
-      <Button onClick={handleBack} variant='contained'>
-        Volver al listado
-      </Button>
+      {loading || user === null? (
+        <h2>Cargando datos...</h2>
+      ) : (
+        <>
+          <CardUserDetail user={user} />
+          <h2>Posteos</h2>
+          <ContainerPosts posts={POSTS_MOCK} />
+          <Button onClick={handleBack} variant='contained'>
+            Volver al listado
+          </Button>
+        </>
+      )}
     </div>
   )
 }
