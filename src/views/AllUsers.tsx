a577f339eb0b2dcd3caa7a87fe346a006e4079e2
ivangonz <ivanezeq.gonzalez@gmail.com>
2025-03-10 08:20:47 -0300

    ITSM-3 se dan los estilos completos a la tabla y pantalla AllUsers

--- src/views/AllUsers.tsx
+++ src/views/AllUsers.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react';
-import TableUsers from '../components/TableUsers/TableUsers';
-import { IUser } from '../domain/User';
-import { USERS_MOCK } from './mocks/users';
-
+import { useState } from 'react'
+import TableUsers from '../components/TableUsers/TableUsers'
+import { IUser } from '../domain/User'
+import { USERS_MOCK } from './mocks/users'
+import './AllUsers.module.css'
 
 const AllUsers = () => {
   const [users, setUsers] = useState<IUser[]>(USERS_MOCK)
-  return <TableUsers users={users}/>
+  return (
+    <div>
+      <h1>Búsqueda de usuarios</h1>
+      <TableUsers users={users} />
+    </div>
+  )
 }
 
-export default AllUsers;
+export default AllUsers
