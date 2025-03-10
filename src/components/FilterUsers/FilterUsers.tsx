baa5ecd0e4acaec9565cc6ef97a1f5d742668f7c
ivangonz <ivanezeq.gonzalez@gmail.com>
2025-03-10 16:13:14 -0300

    ITSM-10 Implementar un buscador que filtre por nombre, usuario, email y empresa

---
+++ src/components/FilterUsers/FilterUsers.tsx
@@ -0,0 +1,78 @@
+import { Autocomplete, Button, TextField } from '@mui/material'
+import { defaultQueries, IQueriesFilterUser } from '../../views/AllUsers'
+import styles from './FilterUsers.module.css'
+import { useState } from 'react'
+
+export type Options = {
+  name: string[]
+  username: string[]
+  email: string[]
+  companyName: string[]
+}
+
+interface IFilterUser {
+  setQueries: (queries: IQueriesFilterUser) => void
+  options: Options
+  clearQueries: () => void
+}
+
+const FilterUsers = (props: IFilterUser) => {
+  const { setQueries, options, clearQueries } = props
+
+  const [temporalQueries, setTemporalQueries] = useState<IQueriesFilterUser>(defaultQueries)
+
+  const handleOnChange = (key: string, value: string) => {
+    setTemporalQueries((prev) => ({ ...prev, [key]: value }))
+  }
+
+  const search = () => {
+    setQueries(temporalQueries)
+  }
+
+  const handleClearQueries = () => {
+    setTemporalQueries(defaultQueries)
+    clearQueries()
+  }
+
+  return (
+    <div className={styles.containerFIlters}>
+      <Autocomplete
+        disablePortal
+        size='small'
+        options={options.name}
+        sx={{ width: 200 }}
+        value={temporalQueries.name ?? ''}
+        onInputChange={(_, value) => handleOnChange('name', value)}
+        renderInput={(params) => <TextField {...params} label='Nombre' />}
+      />
+      <Autocomplete
+        disablePortal
+        size='small'
+        options={options.username}
+        sx={{ width: 200 }}
+        onInputChange={(_, value) => handleOnChange('username', value)}
+        renderInput={(params) => <TextField {...params} label='Nombre de usuario' />}
+      />
+      <Autocomplete
+        disablePortal
+        size='small'
+        options={options.email}
+        sx={{ width: 220 }}
+        onInputChange={(_, value) => handleOnChange('email', value)}
+        renderInput={(params) => <TextField {...params} label='Email' />}
+      />
+      <Autocomplete
+        disablePortal
+        size='small'
+        options={options.companyName}
+        sx={{ width: 200 }}
+        onInputChange={(_, value) => handleOnChange('companyname', value)}
+        renderInput={(params) => <TextField {...params} label='Nombre de empresa' />}
+      />
+      <Button onClick={search}>Buscar</Button>
+      <Button onClick={handleClearQueries}>X</Button>
+    </div>
+  )
+}
+
+export default FilterUsers
--- src/views/AllUsers.tsx
+++ src/views/AllUsers.tsx
@@ -3,22 +3,83 @@ import TableUsers from '../components/TableUsers/TableUsers'
 import { IUser } from '../domain/User'
 import './AllUsers.module.css'
 import { getUsers } from '../services/Users'
+import FilterUsers, { Options } from '../components/FilterUsers/FilterUsers'
+
+export interface IQueriesFilterUser {
+  name?: string
+  username?: string
+  email?: string
+  companyName?: string
+}
+
+export const defaultQueries: IQueriesFilterUser = {}
+const defaultOptions: Options = {
+  name: [],
+  username: [],
+  email: [],
+  companyName: []
+}
+
+const filterArrayOf = (arr: IUser[], key: string): any[] => {
+  return arr.map((element) => element[key])
+}
+
+const getOptions = (users: IUser[]): Options => {
+  const nameOptions = filterArrayOf(users, 'name')
+  const userNameOptions = filterArrayOf(users, 'username')
+  const emailOptions = filterArrayOf(users, 'email')
+  const filteredCompany = filterArrayOf(users, 'company')
+  const companyNameOptions = filterArrayOf(filteredCompany, 'name')
+  return {
+    name: nameOptions,
+    username: userNameOptions,
+    email: emailOptions,
+    companyName: companyNameOptions
+  }
+}
+
+const filterUsers = (users: IUser[], queries: IQueriesFilterUser): IUser[] => {
+  const filteredKeys = Object.entries(queries).filter(([k, v]) => v !== '')
+  let result = users
+  filteredKeys.forEach(([key, value]) => {
+    result = result.filter((user) => user[key] === value)
+  })
+  return result
+}
 
 const AllUsers = () => {
   const [users, setUsers] = useState<IUser[]>([])
   const [loading, setLoading] = useState<boolean>(false)
-
-  useEffect(() => {
+  const [queries, setQueries] = useState<IQueriesFilterUser>(defaultQueries)
+  const [options, setOptions] = useState<Options>(defaultOptions)
+  const searchAllUsers = () => {
     setLoading(() => true)
     getUsers().then((response) => {
       setUsers(response.data)
       setLoading(() => false)
     })
+  }
+
+  const handleClearQueries = () => {
+    setQueries(defaultQueries)
+    searchAllUsers()
+  }
+  useEffect(() => {
+    searchAllUsers()
   }, [])
+
+  useEffect(() => {
+    setOptions(getOptions(users))
+  }, [users])
+
+  useEffect(() => {
+    setUsers(filterUsers(users, queries))
+  }, [queries])
 
   return (
     <div>
       <h1>Búsqueda de usuarios</h1>
+      <FilterUsers setQueries={setQueries} options={options} clearQueries={handleClearQueries} />
       <TableUsers users={users} loading={loading} />
     </div>
   )
