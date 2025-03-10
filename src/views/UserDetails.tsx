e4d7ebbec74a554d10074625866b754b89dfb619
ivangonz <ivanezeq.gonzalez@gmail.com>
2025-03-10 12:36:06 -0300

    ITSM-7 Consumir el service y mostrar los posteos del User

--- src/views/UserDetails.tsx
+++ src/views/UserDetails.tsx
@@ -1,15 +1,17 @@
 import { useNavigate, useParams } from 'react-router'
 import CardUserDetail from '../components/CardUserDetail/CardUserDetail'
 import { Button } from '@mui/material'
-import { POSTS_MOCK } from './mocks/posts'
 import ContainerPosts from '../components/ContainerPosts/ContainerPosts'
 import { useEffect, useState } from 'react'
 import { IUser } from '../domain/User'
 import { getUser } from '../services/Users'
+import { IPost } from '../domain/Post'
+import { getPosts } from '../services/Posts'
 
 const UserDetails = () => {
   const navigate = useNavigate()
   const [user, setUser] = useState<IUser | null>(null)
+  const [posts, setPosts] = useState<IPost[]>([])
   const [loading, setLoading] = useState<boolean>(true)
   const { id } = useParams()
 
@@ -21,6 +23,9 @@ const UserDetails = () => {
     if (!id) return
     getUser(id).then((response) => {
       setUser(response.data)
+    })
+    getPosts(id).then((response) => {
+      setPosts(response.data)
       setLoading(() => false)
     })
   }, [])
@@ -43,7 +48,7 @@ const UserDetails = () => {
         <>
           <CardUserDetail user={user} />
           <h2>Posteos</h2>
-          <ContainerPosts posts={POSTS_MOCK} />
+          <ContainerPosts posts={posts} />
           <Button onClick={handleBack} variant='contained'>
             Volver al listado
           </Button>
